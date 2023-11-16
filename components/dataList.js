17f60add32a496971e409232cf6e5b908489f70f
derwa <audrius.dervinis@gmail.com>
2023-11-16 07:35:30 +0200

    reval path remove

--- components/dataList.js
+++ components/dataList.js
@@ -1,10 +1,8 @@
-import { revalidatePath } from "next/cache"
 import { getXataClient } from "../src/xata"
 const xata = getXataClient()
 
 const DataList = async () => {
     const data = await xata.db.List.getAll()
-    revalidatePath('/')
 
   return (
     <div>
