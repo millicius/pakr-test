15bbc73d3962cdb60aa2e306d935abdf9eba562c
derwa <audrius.dervinis@gmail.com>
2023-11-16 07:10:29 +0200

    pak update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
-export default function Home() {
+import DataList from '@/components/DataList'
+
+export default async function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
       <h1>pakr_data</h1>
+      <DataList />
     </div>
   )
 }
--- components/dataList.js
+++ components/dataList.js
@@ -1,6 +1,18 @@
-const dataList = () => {
+import { getXataClient } from "@/src/xata"
+const xata = getXataClient()
+
+const dataList = async () => {
+    const data = await xata.db.List.getAll()
+
   return (
-    <div>dataList</div>
+    <div>
+        <h1>Data List</h1>
+        {
+            data.map((d, index) => (
+                <p key={index}>{d.name}</p>
+            ))
+        }
+    </div>
   )
 }
 
